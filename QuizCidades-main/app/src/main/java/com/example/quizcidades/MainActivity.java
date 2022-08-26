f1cd1b2bb9352d8ea62f5a8743c9185fe11a6cc1
Claudinei Varela <nei_box@hotmail.com>
2022-08-25 22:52:08 -0300

    agora sim! maldito usesClearTextTraffic...

--- QuizCidades-main/app/src/main/java/com/example/quizcidades/MainActivity.java
+++ QuizCidades-main/app/src/main/java/com/example/quizcidades/MainActivity.java
@@ -15,20 +15,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.InputStream;
 import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    
-    String[] cidades = {"barcelona","brasilia","curitiba","lasvegas","montreal","paris","riodejaneiro","salvador","saopaulo","toquio"};
+
+    String[] cidades = {"barcelona", "brasilia", "curitiba", "lasvegas", "montreal", "paris", "riodejaneiro", "salvador", "saopaulo", "toquio"};
     String[] nomesCorretosCidade = {"Barcelona", "Brasília", "Curitiba", "Las Vegas", "Montreal", "Paris", "Rio de Janeiro", "Salvador", "São Paulo", "Tóquio"};
+    String[] cidadesUrl = {"01_barcelona", "02_brasilia", "03_curitiba", "04_lasvegas", "05_montreal", "06_paris", "07_riodejaneiro", "08_salvador", "09_saopaulo", "10_toquio"};
     ArrayList<Integer> sorteados = new ArrayList<>();
     TextView textViewProgresso, textViewResultado;
     EditText editTextCidade;
     ImageView imageViewCidade;
+
     int sorteado;
     String cidadeSorteada;
     Button button;
@@ -44,57 +45,52 @@ public class MainActivity extends AppCompatActivity {
         editTextCidade = findViewById(R.id.editTextCidade);
         imageViewCidade = findViewById(R.id.imageViewCidade);
 
-
         textViewResultado.setText("");
-
         button = findViewById(R.id.button);
-
-
-
         sortear();
+
     }
 
-    public void adivinhar(View view){
+    public void adivinhar(View view) {
         String resposta = editTextCidade.getText().toString();
-        if(resposta.length()==0){
-            Toast.makeText(this,"Digite uma resposta", Toast.LENGTH_SHORT).show();
-        }else{
+        if (resposta.length() == 0) {
+            Toast.makeText(this, "Digite uma resposta", Toast.LENGTH_SHORT).show();
+        } else {
             resposta = resposta.toLowerCase();
-            resposta = resposta.trim().replace(" ","");
+            resposta = resposta.trim().replace(" ", "");
             resposta = Normalizer.normalize(resposta, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
-            if(resposta.equals(cidadeSorteada)){
+            if (resposta.equals(cidadeSorteada)) {
                 textViewResultado.setText("Você acertou!");
-                pontuacao+=25;
-            }else{
+                pontuacao += 25;
+            } else {
                 textViewResultado.setText("Você errou, a resposta certa era: " + nomesCorretosCidade[sorteado]);
             }
         }
 
-        if(sorteados.size()==4){
+        if (sorteados.size() == 4) {
             editTextCidade.setFocusable(false);
             button.setText("Ver resultado");
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent i = new Intent(MainActivity.this,ResultadoActivity.class);
-                    i.putExtra("resultado",pontuacao);
+                    Intent i = new Intent(MainActivity.this, ResultadoActivity.class);
+                    i.putExtra("resultado", pontuacao);
                     i.setFlags(i.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
                     startActivity(i);
                 }
             });
-        }else {
+        } else {
             editTextCidade.setText("");
             sortear();
         }
     }
 
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
+         ImageView bmImage;
 
         public DownloadImageTask(ImageView bmImage) {
             this.bmImage = bmImage;
         }
-
         protected Bitmap doInBackground(String... urls) {
             String urldisplay = urls[0];
             Bitmap mIcon11 = null;
@@ -107,13 +103,12 @@ public class MainActivity extends AppCompatActivity {
             }
             return mIcon11;
         }
-
         protected void onPostExecute(Bitmap result) {
             bmImage.setImageBitmap(result);
         }
     }
 
-        private void sortear(){
+    private void sortear(){
         Random gerador = new Random();
         sorteado = gerador.nextInt(10);
         while(sorteados.contains(sorteado)){
@@ -122,20 +117,16 @@ public class MainActivity extends AppCompatActivity {
         sorteados.add(sorteado);
         cidadeSorteada = cidades[sorteado];
 
-        int resourceId = getResources().getIdentifier(cidadeSorteada, "drawable",
-                getPackageName());
+        /* Se fosse usar drawable seria por aqui
 
+        int resourceId = getResources().getIdentifier(cidadeSorteada, "drawable", getPackageName());
         imageViewCidade.setImageResource(resourceId);
+        */
 
-        // show The Image in a ImageView
-        //a url do dropbox do professor n funciona https://www.dropbox.com/s/5539hvtjwum4bw9/01_barcelona.jpg
-        // a url abaixo funciona, mas pra fazer o joguinho funcionar, vou comentar.
-
-        //new DownloadImageTask((ImageView) findViewById(R.id.imageViewCidade))
-        //        .execute("https://www.ufpr.br/portalufpr/wp-content/uploads/2022/05/concurso-ufpr-1.jpg");
+        new DownloadImageTask((ImageView) imageViewCidade)
+                .execute("http://200.236.3.202/android/cidades/"+ cidadesUrl[sorteado] + ".jpg");
 
         textViewProgresso.setText(sorteados.size()+"/4");
     }
-
 
 }
